4ab5ddc5b3802df714be70318661187d5aa0a9de
Jack <jack@jkdigital.info>
2021-11-05 07:31:05 +1100

    Reveresed changes to this file, that were not necessary.

--- src/components/blocks/CalltoActionPay.js
+++ src/components/blocks/CalltoActionPay.js
@@ -5,7 +5,7 @@ import Payment from './Payment';
 const CalltoActionPay = ({block}) => {
     
     return (
-        <Container sx={{py: 2, width: 100/100}}>
+        <Container sx={{py: 2}}>
             <Typography sx={{py: 2}} variant="h4" children={block.title} />
             <Typography sx={{py: 2}} children={block.text} />
             <Payment block={block.payment} />
