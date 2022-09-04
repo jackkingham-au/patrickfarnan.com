4426f5ebcd59f36c08d1f8469cecc804eb08994d
Jack Kingham <52394694+jkdigital-au@users.noreply.github.com>
2022-09-04 14:20:02 +1000

    Always Go Far Logo Added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ const Footer = () => {
     return (
         <Box component="footer" sx={{py: 4, px: 1, bgcolor: 'secondary.main', mt: 2}}>
             <Container>
-                <Image src="/assets/patrick-farnan-logo.png" alt="Patrick Farnan Logo" type="logo" />
+                <Image src="/assets/patrick-farnan-logo.jpg" alt="Patrick Farnan Logo" type="logo" styles={{height: '80px', width: 'auto'}} />
                 <Grid container>
                     <Grid item xs={12} md={6}>
                         <List dense sx={{color: '#fff'}}>
@@ -48,11 +48,11 @@ const Footer = () => {
                                 </ListItemIcon>
                                 <ListItemText primaryTypographyProps={{sx: {fontWeight: 'bold'}}}  primary="(+61) 400 813 844" />
                             </ListItemButton>
-                            <ListItemButton component="a" href="mailto:patrick@patrickfarnan.com">
+                            <ListItemButton component="a" href="mailto:patrick@alwaysgofar.com">
                                 <ListItemIcon sx={{color: 'primary.main'}}>  
                                     <EmailIcon />
                                 </ListItemIcon>
-                                <ListItemText primaryTypographyProps={{sx: {fontWeight: 'bold'}}}  primary="patrick@patrickfarnan.com" />
+                                <ListItemText primaryTypographyProps={{sx: {fontWeight: 'bold'}}}  primary="patrick@alwaysgofar.com" />
                             </ListItemButton>
                             <ListItemButton component="a" href="https://www.facebook.com/groups/an.elite.performance.mindset?fbclid=IwAR1UgSVNIBknZYH8SvE0HWnLgcHY0BICBBtXTrRO46o60kh_P8yoWCLD1FE" target="_blank">
                                 <ListItemIcon sx={{color: 'primary.main'}}>  
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import PhoneIcon from '@mui/icons-material/Phone';
 const Header = () => {
     return (
         <Box component="header" sx={{position: 'relative',bgcolor: 'secondary.main', alignItems: 'center', py: 1, px: 2, display: 'flex', zIndex: 1100, justifyContent: 'space-between'}}>
-            <Image type="logo" src="/assets/patrick-farnan-logo.png" alt="Patrick Farnan Logo" />
+            <Image type="logo" src="/assets/patrick-farnan-logo.jpg" alt="Patrick Farnan Logo" styles={{height: '64px', width: 'auto'}} />
             <Stack direction="row" sx={{display: {xs: 'none', sm: 'flex'}}}>
                 <PhoneIcon sx={{color: 'primary.main', mr: 1}} />
                 <Link href="tel:+61400813844" children="(+61) 400 813 844" underline="none" variant="body1" sx={{color: 'common.white'}} />
